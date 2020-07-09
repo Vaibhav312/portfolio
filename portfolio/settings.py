d2d3a5b602c4726c7538e8923814c3e668c4b558
Vaibhav <vaibhavbaghel3196@gmail.com>
2020-07-09 22:12:39 +0530

    Final_commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -71,8 +71,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT='587'
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER=config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER='vaibhavtest321@gmail.com'
+EMAIL_HOST_PASSWORD='123456789aVB$'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
