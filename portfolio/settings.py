aac09663180c9ec3820c6e992a589602b366d80a
Vaibhav <vaibhavbaghel3196@gmail.com>
2020-07-09 17:23:43 +0530

    updated settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,7 +23,7 @@ STATIC_DIR=os.path.join(BASE_DIR,'static')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = "0lxtk)c!r_6$af9f9zc-3go%ah98m_9(ppummx4eqdy(--ro-l"
 print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
